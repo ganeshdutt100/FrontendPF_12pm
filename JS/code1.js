e95af63b6cce49494e8ab64332c9e52c2d83c350
unknown <gdutt2304@gmail.com>
2025-09-17 17:22:56 +0530

    Update Simple if and if else

---
+++ JS/code1.js
@@ -0,0 +1,19 @@
+console.log("Code_1");
+//  if(condition){
+//     stm
+//  }
+
+let a  = 6;
+// if( a > 5){
+//     console.log("a is greater than 5");
+// }
+
+6 > 5//  
+if( a > 5){
+    console.log("a is greater than 5");
+}else{
+    console.log(a);
+    
+}
+
+
