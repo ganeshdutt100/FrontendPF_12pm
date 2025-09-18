fca3e50b1aa78171cfdcbb5eefc019edfbcb2dca
unknown <gdutt2304@gmail.com>
2025-09-18 16:19:36 +0530

    Update IF-ELSE-IF

--- JS/code1.js
+++ JS/code1.js
@@ -8,12 +8,13 @@ let a  = 6;
 //     console.log("a is greater than 5");
 // }
 
-6 > 5//  
-if( a > 5){
-    console.log("a is greater than 5");
-}else{
-    console.log(a);
+//6 > 5
+// if( a > 5){
+//     console.log("a is greater than 5");
+// }else{
+//     console.log(a);
     
-}
+// }
 
+// (condition)?true:false;
 
---
+++ JS/code2.js
@@ -0,0 +1,30 @@
+console.log("CODE2");
+
+// let a = 72;
+// if(a % 2 == 0){
+//     console.log("Even");
+    
+// }else{
+//     console.log("Odd");
+    
+// }
+
+let marks = 49;
+if(marks >= 75){
+    console.log("A");    
+}
+
+else if(marks >= 60){
+    console.log("B");
+}
+else  if(marks >= 50){
+    console.log("C");
+    
+}
+else if(marks >= 30){
+    console.log("D");
+}
+else{
+    console.log("Fail");
+    
+}
