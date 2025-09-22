36d8270b3945f1d58b99c61ef0f4ac247d3334f6
unknown <gdutt2304@gmail.com>
2025-09-22 16:27:42 +0530

    Update Nested IF-Else

--- JS/code2.js
+++ JS/code2.js
@@ -9,22 +9,50 @@ console.log("CODE2");
     
 // }
 
-let marks = 49;
-if(marks >= 75){
-    console.log("A");    
-}
+// let marks = 49;
+// if(marks >= 75){
+//     console.log("A");    
+// }
 
-else if(marks >= 60){
-    console.log("B");
-}
-else  if(marks >= 50){
-    console.log("C");
+// else if(marks >= 60){
+//     console.log("B");
+// }
+// else  if(marks >= 50){
+//     console.log("C");
     
-}
-else if(marks >= 30){
-    console.log("D");
-}
-else{
-    console.log("Fail");
+// }
+// else if(marks >= 30){
+//     console.log("D");
+// }
+// else{
+//     console.log("Fail");
+    
+// }
+
+
+// let a = 16;
+// let b = 11;
+// if(a > b){
+//     console.log(a);
+    
+// }else{
+//     console.log(b);
+    
+// }
+// let year  = 2024
+// if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+//     console.log("Leap year ");
+    
+// }else{
+//     console.log("Not a leap year");
+    
+// }
+// 1900
+let year  = 1900
+
+if((year % 4 == 0 && year % 100 !== 0) ||(year % 400 == 0)){
+    console.log("Leap year");
+}else{
+    console.log("Not a leap year");
     
 }
---
+++ JS/code3.js
@@ -0,0 +1,39 @@
+// console.log("Nested If-Else");
+
+// let a = 8;
+// let b = 7
+
+// if(a%2 ==0){
+//     console.log("Even ");
+
+//      if(b>= 10){
+//         console.log(b);
+        
+//     }else{
+//         console.log("data not found");
+        
+//     }
+   
+    
+// }else{
+//     console.log("odd");
+    
+// }
+
+let age  = 17;
+let hasLicense=  true;
+
+if(age >= 18){
+    console.log("your age  enough for driving license");
+    if(hasLicense){
+        console.log("You have a license");
+        
+    }else{
+      console.log("you don`t have a license");
+      
+    }
+    
+}else{
+    console.log("your age not enough for driving license");
+    
+}
