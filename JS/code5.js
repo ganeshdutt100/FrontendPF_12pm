465fda76e18c232735d363e17a42b53b881a0813
unknown <gdutt2304@gmail.com>
2025-09-23 17:38:40 +0530

    function Question

--- JS/code5.js
+++ JS/code5.js
@@ -49,3 +49,15 @@
 //  arrowFun()
  
 
+// function fun(ch){
+    
+// if(ch === "a" || ch === "e" || ch === "i"|| ch === "o"|| ch === "u"){
+// console.log("Vowel");
+
+// }else{
+//     console.log("Consonant");
+    
+// }
+// }
+
+// fun('b')
