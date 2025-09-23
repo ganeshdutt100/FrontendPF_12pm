5b0b2089d31d556829bfb0614d2f6288a1478c5a
unknown <gdutt2304@gmail.com>
2025-09-23 17:16:23 +0530

    Update Function

---
+++ JS/code4.js
@@ -0,0 +1,34 @@
+// Write a program to check the type of triangle (Equilateral, Isosceles, or Scalene) based on sides.
+// let a  = 5
+// let b  = 7
+// let c  = 0
+
+// if(a + b > c && a+c > b && b+c > a){
+//     if(a === b && b === c){
+//     console.log("Equilateral triangle ");
+
+//     }
+
+//     else if(a === b || b === c || a ===c){
+//         console.log("Isosceles");
+        
+//     }else{
+//         console.log("Scalene");
+        
+//     }
+// } else{
+//     console.log("Not a valid Triangle ");
+    
+// }
+// Write a program to check whether a character is a vowel or consonant
+
+let ch = "g";
+
+
+if(ch === "a" || ch === "e" || ch === "i"|| ch === "o"|| ch === "u"){
+console.log("Vowel");
+
+}else{
+    console.log("Consonant");
+    
+}
---
+++ JS/code5.js
@@ -0,0 +1,14 @@
+// function fun(){
+//     console.log("Hey i am function ");    
+// }
+// fun();
+
+// function fun1(a,b){
+//  return console.log( "The sum of two numbers is " + (a+b));
+    
+// }
+// fun1(10,20);
+
+
+
+
