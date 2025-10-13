d9db6df652762ec31919721646814e5e9120233e
unknown <gdutt2304@gmail.com>
2025-10-13 15:34:57 +0530

    array

---
+++ JS/code11.js
@@ -0,0 +1,26 @@
+// let student1 = "Value1";
+// let student2 = "Value2";
+// let student3 = "Value3";
+// let student4 = "Value4";
+// let student5 = "Value5";
+// let student6 = "Value6";
+// let student7 = "Value7";
+// let student8 = "Value8";
+// let student9 = "Value9";
+// let student10 = "Value10";
+
+// console.log(student1, student2, student3);
+
+let students = ["value1", "value2", "value3"];
+console.log(students[1]);
+
+let value1 = new Array();
+value1[0] = "Orange";
+value1[1] = "Apple";
+value1[2] = "Banana";
+value1[3] = "Grapes";
+
+console.log(value1);
+
+let cars = new Array("XUV", "Swift", "Verna");
+console.log(cars);
