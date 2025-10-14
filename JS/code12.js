7cabad085a3e019923a61e5572d624e5ffcfe8ee
unknown <gdutt2304@gmail.com>
2025-10-14 16:29:57 +0530

    Array ,  Mutator , Accessor metthods

---
+++ JS/code12.js
@@ -0,0 +1,32 @@
+console.log("Code_12");
+// Mutator Methods:
+// let arr = [1, 2, 12, 5, 7, 8];
+// arr.fill("JavaScript", 0, 1);
+// arr.pop();
+// arr.push("JavaScript");
+// arr.reverse();
+// arr.shift();
+// arr.unshift("JavaScript");
+// arr.sort();
+// arr.splice(2, 3, "String");
+// console.log(arr);
+
+// let fruits = ["apple", "banana", "orange", "kiwi", "grape"];
+// fruits.copyWithin(2, 0, 4);
+// console.log(fruits);
+
+// 2 - target - orange;
+// "apple", "banana", apple", "banana", "orange"
+//--------------------------- Accessor Methods:
+
+let arr1 = [1, 2, 3, 4, 5, 6, 1];
+let arr2 = [11, 21, 31, 41, 51, 61];
+
+// let output = arr1.concat(arr2);
+// let output = arr1.includes(1);
+// let output = arr1.indexOf(1);
+// let output = arr1.lastIndexOf(1);
+// let output = arr1.join(" | ");
+// let output = arr1.slice(2, 5);
+let output = arr1.toString();
+console.log(output);
