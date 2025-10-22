e7f1c77ab5871fab146af53afdc2b6f44935a8cf
unknown <gdutt2304@gmail.com>
2025-10-22 19:25:27 +0530

    Update String

---
+++ JS/code13.js
@@ -0,0 +1,24 @@
+// let a = "Ganesh Dutt";
+// let value = 3443948;
+// let b = `
+// Ganesh
+// Dutt ${value}`;
+// console.log(b);
+
+// let a = "Hello";
+// let b = "World";
+// console.log(a, " ", b);
+// console.log(`${a}  ${b}`);
+
+// let value = " Hello  world ";
+// console.log(value.length);
+// console.log(value.toLowerCase());
+// console.log(value.indexOf());
+// console.log(value.indexOf("w"));
+// console.log(value.substring(0, 4));
+// console.log(value.split("  "));
+// console.log(value.length);
+// let newValue = value.trim();
+// console.log(newValue.length);
+// console.log(value.lastIndexOf("o"));
+// console.log(value.replace(/o/g, "a"));
