f897f574173d89f90ee032588a40846fb53f2144
unknown <gdutt2304@gmail.com>
2025-10-28 16:11:20 +0530

    Promise Chaining project

--- JS/code17.js
+++ JS/code17.js
@@ -1,3 +1,32 @@
-setTimeout(() => {
-  console.log("Hello World");
-}, 3000);
+// setTimeout(() => {
+//   console.log("Hello World");
+// }, 3000);
+function orderPizza() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Pizza Ordered");
+      resolve("Pizza");
+    }, 1000);
+  });
+}
+function deliverPizza(pizza) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Pizza Delivered");
+      resolve(pizza + "Delivered");
+    }, 1000);
+  });
+}
+function makePayment(order) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Payment Done");
+      resolve(order + "Paid");
+    }, 1000);
+  });
+}
+orderPizza()
+  .then((pizza) => deliverPizza(pizza))
+  .then((order) => makePayment(order))
+  .then((finalResult) => console.log("Completed"))
+  .catch((err) => console.log(err));
