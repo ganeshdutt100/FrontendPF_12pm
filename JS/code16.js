6261072d6622b74dddce3e8d9871f29ec212d8eb
unknown <gdutt2304@gmail.com>
2025-10-28 15:57:27 +0530

    Promise Chaining

--- JS/code16.js
+++ JS/code16.js
@@ -1,13 +1,32 @@
 // const myPromise = new Promise((resolve, reject) => {});
 // myPromise.then();
-const myPromise = new Promise((resolve, reject) => {
-  let a = 2;
-  if (a % 2 === 0) {
-    resolve("Even");
-  } else {
-    reject("Odd");
-  }
+// const myPromise = new Promise((resolve, reject) => {
+//   let a = 2;
+//   if (a % 2 === 0) {
+//     resolve("Even");
+//   } else {
+//     reject("Odd");
+//   }
+// });
+// myPromise
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+let promiseValue = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 1000);
 });
-myPromise
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+
+promiseValue
+  .then((result) => {
+    console.log(result); // 10
+    return result + 1;
+  })
+  .then((result) => {
+    console.log(result); // 11
+    return result * 2;
+  })
+  .then((result) => {
+    console.log(result); // 22
+  });
