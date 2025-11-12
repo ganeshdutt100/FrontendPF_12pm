209b181a36d76dea78608ff72fa59ad74bd0686c
unknown <gdutt2304@gmail.com>
2025-11-12 10:32:23 +0530

    props

--- React/project2/src/App.jsx
+++ React/project2/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Card from './components/Card'
 import { Code1 } from './components/Code1'
 import HeroSection from './components/HeroSection'
 import MyNavbar from './components/Navbar'
@@ -12,11 +13,17 @@ function App() {
     <>
 
 
-      <MyNavbar logo="Ganesh Dutt" />
-      <MyNavbar logo="Logo Name" />
-      <MyNavbar logo="GYM" />
+      {/* <MyNavbar logo="Ganesh Dutt" /> */}
+      {/* <MyNavbar logo="Logo Name" /> */}
+      {/* <MyNavbar logo="GYM" /> */}
       {/*  <HeroSection /> */}
       {/* <Code1 /> */}
+      <Card heading="Card1" CATEGORY="CATEGORY1" />
+      <Card heading="Card2" CATEGORY="CATEGORY2" />
+      <Card heading="Card3" CATEGORY="CATEGORY3" />
+      <Card heading="Card4" CATEGORY="CATEGORY4" />
+      <Card heading="Card5" CATEGORY="CATEGORY5" />
+
 
 
 
