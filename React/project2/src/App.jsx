81afb6f47be6e7bc16daff20e49d1545da1d142c
unknown <gdutt2304@gmail.com>
2025-11-17 09:43:01 +0530

    Update cards with map

--- React/project2/src/App.jsx
+++ React/project2/src/App.jsx
@@ -5,6 +5,30 @@ import { Code1 } from './components/Code1'
 import HeroSection from './components/HeroSection'
 import MyNavbar from './components/Navbar'
 
+// let value = ["Aditya", "Amar", "Sonu"];
+
+
+let users = [
+  {
+    name: "Aditya",
+    course: "Frontend",
+    image: "https://images.unsplash.com/photo-1749627995669-4d4dda3a9c1d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ad."
+  },
+  {
+    name: "Sonu",
+    course: "JavaFullStack",
+    image: "https://images.unsplash.com/photo-1763063462165-94125cccf210?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ad."
+  },
+  {
+    name: "Amar",
+    course: "mernStack",
+    image: "https://plus.unsplash.com/premium_photo-1720660750856-8f7dc0643a8f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ad."
+  },
+
+]
 
 function App() {
 
@@ -12,17 +36,32 @@ function App() {
   return (
     <>
 
+      {/* <h2>{names}</h2> */}
 
+      {/* <div>
+
+        {users.map((x) => (
+          <h2>{x.name} :  {x.course} : {x.desc}</h2>
+
+        ))} */}
+      <div>
+
+        {users.map((user, index) => (
+          <Card key={index} name={user.name} course={user.course} image={user.image} desc={user.desc}></Card>
+
+        ))}
+        {/* <Card /> */}
+      </div>
       {/* <MyNavbar logo="Ganesh Dutt" /> */}
       {/* <MyNavbar logo="Logo Name" /> */}
       {/* <MyNavbar logo="GYM" /> */}
       {/*  <HeroSection /> */}
       {/* <Code1 /> */}
-      <Card heading="Card1" CATEGORY="CATEGORY1" />
+      {/* <Card heading="Card1" CATEGORY="CATEGORY1" />
       <Card heading="Card2" CATEGORY="CATEGORY2" />
       <Card heading="Card3" CATEGORY="CATEGORY3" />
       <Card heading="Card4" CATEGORY="CATEGORY4" />
-      <Card heading="Card5" CATEGORY="CATEGORY5" />
+      <Card heading="Card5" CATEGORY="CATEGORY5" /> */}
 
 
 
--- React/project2/src/components/Card.jsx
+++ React/project2/src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({ name, course, image, desc }) => {
     return (
         <div>
             <div className="p-4 md:w-1/3">
                 <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://images.unsplash.com/photo-1749627995669-4d4dda3a9c1d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600" alt="blog" />
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={image} alt="blog" />
                     <div className="p-6">
-                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{props.CATEGORY}</h2>
-                        <h1 className="title-font text-lg font-medium text-gray-100 mb-3">{props.heading}</h1>
-                        <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{name}</h2>
+                        <h1 className="title-font text-lg font-medium text-gray-100 mb-3">{course}</h1>
+                        <p className="leading-relaxed mb-3">{desc}</p>
                         <div className="flex items-center flex-wrap">
                             <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
